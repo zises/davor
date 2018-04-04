93db757a0ac299ba60a5a577061b41e0ab337f13
mrublev <mrublev@htmlbox.net>
2018-04-04 09:46:25 +0300

    Fix Jekill issue

--- _site/js/main-update.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    var BV = new $.BigVideo({useFlashForFirefox:false});
-	BV.init();
-    BV.show([
-        { type: "video/mp4",  src: "vids/river.mp4" },
-        { type: "video/webm", src: "vids/river.webm" },
-        { type: "video/ogg",  src: "vids/river.ogv" }
-    ]);
-});
